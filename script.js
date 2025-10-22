ae268363c5a6f27996dfba87452d2b5f869d365e
GiovBerlato <giovanniberlato7@gmail.com>
2025-10-22 17:29:40 -0300

    Bugfix: Remoção do carregamento dinâmico do head.html, devido à problemas de FOUC (Flash of Unstyled Content), agora as tags do head devem ser copiadas manualmente para as novas páginas.
    Mudança da cor de fundo das páginas para um bege claro.

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-
-// Carrega o head do head.html para todas os outros arquivos HTML.
-fetch('head.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('head-placeholder').innerHTML = data;
-});
-
 // Carrega o cabeçalho do header.html para todas os outros arquivos HTML.
 fetch('header.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
-
+    document.querySelector('header').style.visibility = 'visible'; // Torna o header visível assim que for carregado.
 
     // Adiciona funcionalidade ao menu sanduíche.
     const menuSanduiche = document.querySelector('.menu-sanduiche');
     const menuOffScreen = document.querySelector('.menu-off-screen');
 
-    menuSanduiche.addEventListener('click', () => {
-    menuSanduiche.classList.toggle('active');
-    menuOffScreen.classList.toggle('active');
-});
+    // Só adiciona o listener se o menu existir nesta página
+    if (menuSanduiche && menuOffScreen) {
+        menuSanduiche.addEventListener('click', () => {
+            menuSanduiche.classList.toggle('active');
+            menuOffScreen.classList.toggle('active');
+        });
+    }
 });
 
 fetch('rodape.html')
